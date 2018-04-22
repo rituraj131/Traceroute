1767d01326d6d263c5d98e4075fb18425562ba61
Ritu Raj <ritu.raj@thesilkenswan.com>
2018-04-21 22:46:57 -0500

    sendICMP implemeneted

--- Traceroute/main.cpp
+++ Traceroute/main.cpp
@@ -1,7 +1,29 @@
 #include "common.h"
+#include "Socket.h"
 
 int main(int argc, char **argv) {
-	printf("Hello world!\n");
+	if (argc != 2) {
+		printf("Usage IP/Host address required. Exiting!!\n");
+		return 0;
+	}
+
+	char *host = argv[1];
+	printf("host %s\n", host);
+
+	WSADATA wsaData;
+
+	if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
+		cout << "WSAStartup failed with error : " << WSAGetLastError() << " Exiting!"<<endl;
+		WSACleanup();
+		return -1;
+	}
+
+	Socket mySocket;
+	mySocket.initSocket();
+
+
+
+	WSACleanup();
 	system("pause");
 	return 0;
 }
